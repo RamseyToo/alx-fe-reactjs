7fb267d8faed54f44bba81873df0ce6723bda652
RamseyToo <ramseykiprotichtoo@gmail.com>
2025-11-05 19:29:08 +0300

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,21 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './components/WelcomeMessage'
 import WelcomeMessage from './components/WelcomeMessage'
-
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import { Footer } from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className='h2'>
+      <Header />
+    </div>
       <div>
         <h1 className='h1'>
           Thi is my first js project
-          <WelcomeMessage />
+         
         </h1>
         < WelcomeMessage />
       </div>
+      <main>
+        <MainContent />
+      </main>
       <WelcomeMessage />
+      <footer>
+        <Footer />
+      </footer>
+
     </>
+
   )
 }
 
